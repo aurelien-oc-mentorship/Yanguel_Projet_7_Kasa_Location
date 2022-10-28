85bbba7b504cf3aa2426fd8f5a925a3c07986f3a
yanguel <yanguelcornillet@yahoo.com>
2022-10-28 12:05:03 +0200

    Organisation des fichiers et suppression
    - Création des dossiers "Home" / "pages" et "components"
    - Déplacement du fichier "app.js" dans le dossier Home et renommage en "index.jsx"
    - Renommage du fichier "index.js" Racine en "index.jsx"
    - Suppression des fichier test

---
+++ kasa_location/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {
+          //<img src={logo} className="App-logo" alt="logo" />
+        }
+        <p>ICI C'est la page d'acceuil </p>
+      </header>
+    </div>
+  )
+}
+
+export default Home
