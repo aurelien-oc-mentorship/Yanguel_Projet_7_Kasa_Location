c6435f5a7f65cbc460280fdce436a6bd28700250
yanguel <yanguelcornillet@yahoo.com>
2022-11-01 22:02:11 +0100

    Rajout de la police d'écriture "Montserrat"
    -Rajout de la police d'écriture "Montserra"
    -Modification de plusieurs point css

--- kasa_location/src/pages/Error/index.jsx
+++ kasa_location/src/pages/Error/index.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom'
 function Error() {
   return (
     <div className="containerError">
-      <img src={LogoError} className="logoError" alt="Erreur404" />
+      <img
+        src={LogoError}
+        className="logoError"
+        alt="Erreur 404, la page n'existe pas."
+      />
       <h1 className="textError">
         Oups! La page que vous demandez n'existe pas.
       </h1>
