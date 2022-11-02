ec2452dec5c05ec89f7c95cec84d0de92413c812
yanguel <yanguelcornillet@yahoo.com>
2022-11-02 17:31:26 +0100

    Création des éléments de la page " A propos" sans qu'ils soient déroulant

--- kasa_location/src/pages/apropos/index.jsx
+++ kasa_location/src/pages/apropos/index.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import imgApropos from '../../assets/imageApropos.png'
 import '../../styles/aproposPage.css'
-
+import Vectordown from '../../assets/Vectordown.png'
 function Apropos() {
   return (
-    <section>
+    <section className="containerApropos">
       <div className="bannerApropos">
         <img src={imgApropos} className="imgBanner" alt="Un paysage" />
+      </div>
+
+      <div className="listeApropos">
+        <div className="fiabilite">
+          <p className="textFiabilite"> Fiabilité </p>
+          <img
+            src={Vectordown}
+            className="Vectordown"
+            alt="fleche vers le bas"
+          ></img>
+        </div>
+        <div className="respect">
+          <p className="textRespect">Respect </p>
+          <img
+            src={Vectordown}
+            className="Vectordown"
+            alt="fleche vers le bas"
+          ></img>
+        </div>
+        <div className="service">
+          <p className="textService"> Service </p>
+          <img
+            src={Vectordown}
+            className="Vectordown"
+            alt="fleche vers le bas"
+          ></img>
+        </div>
+        <div className="responsabilite">
+          <p className="textResponsabilite"> Responsabilité </p>
+          <img
+            src={Vectordown}
+            className="Vectordown"
+            alt="fleche vers le bas"
+          ></img>
+        </div>
       </div>
     </section>
   )
